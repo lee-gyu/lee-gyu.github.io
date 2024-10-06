96561ca24e45f9e0a404854e5e97b7aacc97fe40
lee-gyu <leegyu4dev@gmail.com>
2024-10-06 19:23:08 +0900

    feat: update main

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type { import("tailwindcss").Config } */
+const config = {
+    content: ["./src/**/*.{astro,html,tsx,ts}"],
+    theme: {
+        extend: {},
+    },
+};
+
+export default config;
