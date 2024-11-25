14afd79ce581a96c14d82640907b6718a6fae310
lee-gyu <leegyu4dev@gmail.com>
2024-11-25 22:54:29 +0900

    build: add astro/mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
     outDir: "./dist",
     base: "/",
-    integrations: [tailwind(), react()],
+    integrations: [tailwind(), react(), mdx()],
     vite: {},
 });
