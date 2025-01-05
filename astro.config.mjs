cc1af01aa181c8fd989022cf21680a0d72c042a2
lee-gyu <leegyu4dev@gmail.com>
2025-01-05 19:25:54 +0900

    build: add svelte config, and upgrade deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
     outDir: "./dist",
     base: "/",
-    integrations: [tailwind(), react(), mdx()],
+    integrations: [tailwind(), react(), mdx(), svelte()],
     vite: {},
 });
