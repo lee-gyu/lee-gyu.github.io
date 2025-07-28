d7e1749ecc3ce7a3b2ac395de513511f912a02ae
Lee <leegyu4dev@gmail.com>
2025-07-29 00:10:04 +0900

    design: update contents, layout, style

--- panda.config.ts
+++ panda.config.ts
@@ -9,9 +9,14 @@ const globalCss = defineGlobalStyles({
         background: "{colors.background}",
         color: "{colors.text}",
         minWidth: "360px",
+        lineHeight: "1.5",
     },
     "*": {
         transition: "background-color 0.2s ease-in-out",
+    },
+    "*::selection": {
+        backgroundColor: "{colors.primaryActive}",
+        color: "{colors.primary}",
     },
 });
 
@@ -67,6 +72,18 @@ export default defineConfig({
                         value: {
                             base: "{colors.slate.600}",
                             _dark: "{colors.white}",
+                        },
+                    },
+                    primary: {
+                        value: {
+                            base: "{colors.zinc.50}",
+                            _dark: "{colors.zinc.950}",
+                        },
+                    },
+                    primaryActive: {
+                        value: {
+                            base: "{colors.zinc.950}",
+                            _dark: "{colors.zinc.50}",
                         },
                     },
                 },
