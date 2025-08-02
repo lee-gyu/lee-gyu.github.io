ce976939b9fdcca4a33f8726de054998b8c4ec7d
Lee <leegyu4dev@gmail.com>
2025-08-02 15:35:53 +0900

    feat: lastUpdated

--- src/solid/components/LastUpdated.tsx
+++ src/solid/components/LastUpdated.tsx
@@ -1,1 +1,22 @@
-export function LastUpdated() {}
+import dayjs from "dayjs";
+import { getDocLang, useTranslations } from "src/i18n/utils";
+import { css } from "src/styled-system/css";
+import { fromNow } from "src/utils/date";
+
+const lastUpdatedCss = css({
+    fontSize: "0.875rem",
+    opacity: 0.8,
+    fontWeight: 300,
+});
+
+export function LastUpdated() {
+    const lang = getDocLang();
+    const lastUpdated = fromNow(dayjs(LAST_UPDATED), lang);
+    const t = useTranslations(lang);
+
+    return (
+        <span
+            class={lastUpdatedCss}
+        >{`${t("lastUpdated")} ${lastUpdated}`}</span>
+    );
+}
