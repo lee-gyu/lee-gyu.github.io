1826e0e4ab97959159c6815d56faa1fc5e7a283a
Lee <leegyu4dev@gmail.com>
2025-08-24 17:50:38 +0900

    fix: UTC timezone issue

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -3,12 +3,16 @@ import dayjs, { type Dayjs } from "dayjs";
 import "dayjs/locale/ko";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import relativeTime from "dayjs/plugin/relativeTime";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export function format(day: Dayjs | Date, lang: string): string {
-    return dayjs(day).locale(lang).format("llll");
+    return dayjs(day).locale(lang).tz("Asia/Seoul").format("llll");
 }
 
 export function fromNow(day: Dayjs | Date, lang: string): string {
